63f49ecc972fd4a1d44abc560f4feffdbfdbe28a
Seyed Mostafa Hashemian <shashemi@c1r17s2.42madrid.com>
2024-09-28 14:14:35 +0200

    HABIBI

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shashemi <shashemi@student.42madrid.com>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-09-28 12:11:27 by shashemi          #+#    #+#             */
+/*   Updated: 2024-09-28 12:11:27 by shashemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+
+#endif
